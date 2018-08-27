3cc9324b55805dd9e9bea0806110f4b0e23b09ea
Mustafa Cem Karay <cemkaray07@gmail.com>
2018-08-26 23:34:15 -0700

    initial design

--- App.tsx
+++ App.tsx
@@ -1,22 +1,59 @@
 import * as React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {Image, StyleSheet, View} from "react-native";
+import {Card, Header, List, Text, ListItem} from "react-native-elements";
 
 export default class App extends React.Component {
-  public render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-      </View>
-    );
-  }
+    public render() {
+        return (
+            <View style={styles.container}>
+                <Header backgroundColor={"#2b98f0"}
+                        leftComponent={{icon: 'menu', color: '#fff'}}
+                        centerComponent={<Logo/>}
+                        rightComponent={{icon: 'home', color: '#fff'}}
+                />
+
+                <Card title="CURRENT BALANCE">
+                    <Text h3 style={styles.title}>$ 10.288 USD</Text>
+                </Card>
+                    <List>
+                        <ListItem
+                            key={1}
+                            roundAvatar
+                            title={"Bitcoin"}
+                        />
+                        <ListItem
+                            key={2}
+                            roundAvatar
+                            title={"Ethereum"}
+                        />
+                        <ListItem
+                            key={3}
+                            roundAvatar
+                            title={"Ripple"}
+                        />
+                    </List>
+
+
+            </View>
+        );
+    }
+}
+
+
+class Logo extends React.Component {
+    public render() {
+        return (
+            <Image source={require('./images/logo.png')}/>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    alignItems: "center",
-    backgroundColor: "#ff7",
-    flex: 1,
-    justifyContent: "center",
-  },
+    container: {
+        backgroundColor: "#FAFAFB",
+        flex: 1,
+    },
+    title: {
+        textAlign: "center",
+    },
 });
